8514965dee4a912c00b200f55b44b6c21793b68b
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-07-25 23:15:30 +0300

    0.0.25
    fix

--- Packages/Frontend/Api/Units/Telegram/Telegram.js
+++ Packages/Frontend/Api/Units/Telegram/Telegram.js
@@ -1,48 +1,55 @@
-import '../Telegram_api/Telegram_api.js'
+// import './_Telegram_api.js';
+import './Telegram_api.js';
+
+import {ExternalPromise} from '../ExternalPromise/ExternalPromise.js';
+// import {ExternalPromise} from '../../../Global/Js/ExternalPromise/ExternalPromise.js';
 
 
 export class Telegram {
-    static _callback = function(error, success) {
-        this._result = error ? error : success;
-    };
+    // static _callback = (error, success) => {
+    //     this._result = error ? error : success;
+    // };
 
     static _result = null;
     static _telegram = window.Telegram;
-    static _web_app = this._telegram.WebApp;
+    static _webApp = this._telegram.WebApp;
 
 
-    static data__get(key) {
-        this._web_app.CloudStorage.getItem(key, this._callback);
+    static async data__get(key) {
+        let promise = new ExternalPromise();
+        this._webApp.CloudStorage.getItem(key, (error, success) => error ? promise.reject(error) : promise.fulfill(success));
 
-        return this._result;
+        return promise;
     }
 
-    static data__remove() {
-        this._web_app.CloudStorage.removeItems(Array.from(arguments), this._callback);
+    static async data__remove(...args) {
+        let promise = new ExternalPromise();
+        this._webApp.CloudStorage.removeItems(args, (error, success) => error ? promise.reject(error) : promise.fulfill(success));
 
-        return this._result;
+        return promise;
     }
 
-    static data__save(key, data) {
-        this._web_app.CloudStorage.setItem(key, value, this._callback);
+    static async data__save(key, data) {
+        let promise = new ExternalPromise();
+        this._webApp.CloudStorage.setItem(key, value, (error, success) => error ? promise.reject(error) : promise.fulfill(success));
 
-        return this._result;
+        return promise;
     }
 
     static app__init() {
-       this._web_app.expand();
-       this._web_app.ready();
+       this._webApp.expand();
+       this._webApp.ready();
     }
 
-    static user_tg_id__get() {
-        return this._web_app.initDataUnsafe.user.id;
+    static user_id__get() {
+        return this._webApp.initDataUnsafe.user.id;
     }
 
     static verticalSwipes__disable() {
-        this._web_app.disableVerticalSwipes();
+        this._webApp.disableVerticalSwipes();
     }
 
     static verticalSwipes__enable() {
-        this._web_app.enableVerticalSwipes();
+        this._webApp.enableVerticalSwipes();
     }
 }
--- Packages/Frontend/Components/Friends/Friends.js
+++ Packages/Frontend/Components/Friends/Friends.js
@@ -70,44 +70,44 @@ export class Friends extends Component {
 
 
     referals__add() {
-        // this._elements.table.pages_records = [
-        //     {
-        //         "avatar_url": "./logo.jpg",
-        //         "leval": "2",
-        //         "name": "User User",
-        //         "bonus": "200 золота",
-        //         "profit": "1"
-        //     },
-        //     {
-        //         "avatar_url": "#",
-        //         "leval": "3",
-        //         "name": "2",
-        //         "bonus": "bonus",
-        //         "profit": "1"
-        //     },
-        //     {
-        //         "avatar_url": "#",
-        //         "leval": "4",
-        //         "name": "3",
-        //         "bonus": "bonus",
-        //         "profit": "1"
-        //     },
-        //     {
-        //         "avatar_url": "#",
-        //         "leval": "5",
-        //         "name": "4",
-        //         "bonus": "bonus",
-        //         "profit": "1"
-        //     },
-        //     {
-        //         "avatar_url": "#",
-        //         "leval": "6",
-        //         "name": "5",
-        //         "bonus": "bonus",
-        //         "profit": "1"
-        //     }
-        // ];
-        // this._elements.table.refresh();
+        this._elements.table.pages_records = [
+            {
+                "avatar_url": "./logo.jpg",
+                "leval": "2",
+                "name": "User User",
+                "bonus": "200 золота",
+                "profit": "1"
+            },
+            {
+                "avatar_url": "#",
+                "leval": "3",
+                "name": "2",
+                "bonus": "bonus",
+                "profit": "1"
+            },
+            {
+                "avatar_url": "#",
+                "leval": "4",
+                "name": "3",
+                "bonus": "bonus",
+                "profit": "1"
+            },
+            {
+                "avatar_url": "#",
+                "leval": "5",
+                "name": "4",
+                "bonus": "bonus",
+                "profit": "1"
+            },
+            {
+                "avatar_url": "#",
+                "leval": "6",
+                "name": "5",
+                "bonus": "bonus",
+                "profit": "1"
+            }
+        ];
+        this._elements.table.refresh();
     }
 
     refresh() {
